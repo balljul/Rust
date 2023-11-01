898766b3a37b0157601e0de5671a08440689a797
balljul <juliusball197@gmail.com>
2023-11-01 21:09:06 +0100

    CHANGED the expect to a match expression so i can ignore errors when wrong values are passed in

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,15 +15,18 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read your input");
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please enter a valid number");
+        let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
 
         match guess.cmp(&number_to_guess){
             Ordering::Less => println!("Guess Higher"),
             Ordering::Greater => println!("Guess lower"),
-            Ordering::Equal => println!("You guessed right"),
+            Ordering::Equal => {
+                println!("You guessed right");
+                break;
+            }
         } 
     }
 
