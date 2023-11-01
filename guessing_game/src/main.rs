a212be53a51470ebf6c72c0271a9ed42de99455b
balljul <juliusball197@gmail.com>
2023-11-01 20:57:55 +0100

    UPDATED the game to run in a loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,26 +5,26 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess a number!");
     
-    let mut guess = String::new();
     let number_to_guess = rand::thread_rng().gen_range(1..=5); 
 
-    println!("Please input your guess: ");
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess: ");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read your input");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read your input");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please enter a valid number");
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please enter a valid number");
 
-    println!("Your number was: {guess}");
-
-    match guess.cmp(&number_to_guess){
-        Ordering::Less => println!("Guess Higher"),
-        Ordering::Greater => println!("Guess lower"),
-        Ordering::Equal => println!("You guessed right"),
-    } 
+        match guess.cmp(&number_to_guess){
+            Ordering::Less => println!("Guess Higher"),
+            Ordering::Greater => println!("Guess lower"),
+            Ordering::Equal => println!("You guessed right"),
+        } 
+    }
 
 }
