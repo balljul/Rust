7fce43c01427041e1d139c5ee9a3b873b52ffbe1
balljul <juliusball197@gmail.com>
2023-11-04 13:11:35 +0100

    TESTED loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let number: u32 = 6;
+
+    if number == 5 {
+        println!("Condition is true");
+    } else {
+        println!("Condition is false")
+    }
+    
+    let mut counter: u16 = 0;
+    loop {
+        println!("{counter}");
+        counter = counter + 1;
+        if counter == 20 {
+            break;
+        }
+    }
+
+    println!("Result: {counter}")
+
 }
