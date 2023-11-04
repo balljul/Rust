8e6281bc669a0ccbb6c8db29c95b744cace7db18
balljul <juliusball197@gmail.com>
2023-11-04 13:26:09 +0100

    TESTED loop labels

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn _loop_testing() {
     let number: u32 = 6;
 
     if number == 5 {
@@ -17,7 +17,29 @@ fn main() {
             }
         }
     };
-
     println!("Result: {result}")
 
 }
+
+fn main() {
+
+    'capital_loop: loop { 
+        let kapitel = 3;
+        let mut subcap = 0;
+        
+        '_subcapitel_loop: loop {
+            println!("Kapitel: {kapitel}.{subcap}");
+            subcap += 1;
+            if subcap == 17 {
+                break 'capital_loop;
+            }
+        }
+        
+    }
+
+
+}
+
+// Notes to myself:
+// Loop Labels are usefull if you want to break or continue a loop at a higher scope
+// If conditions without the () takes a bit of getting used to
