49ecc6fe6f8ac7b513ba3bd2d30782c052c9d3c0
balljul <juliusball197@gmail.com>
2023-11-05 19:19:21 +0100

    TESTED more of the ownership and borrowing principel and when variables get copied and when moved

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn _string_ownerships() {
 // Strings:
 
     { // Varablie string_example doesnt exist
@@ -13,6 +13,30 @@ let s3 = s2.clone();
 println!("s1 is not valid anymore.\ns2: {}\ns3: {}\n", s2, s3);
 
 }
+
+fn give_me_string(random_string: String) {
+    // The strings value getx moved to the variable random string
+    println!("The string you gave me is: {}\n", random_string);
+    // Here the scope ends and the mem gets freed and the string from is invalid
+}
+
+fn give_me_integer(random_int: i32) {
+    // The intereger just gets copied so its still valid in the main function
+    println!("The int you gave me is: {}\n", random_int);
+    // Here the int gets dropped and nothing special happens
+}
+
+fn main() {
+    let x = 5;
+    let y = x;
+
+    let mystr = String::from("Test12345");
+
+    give_me_string(mystr);
+    give_me_integer(y);
+
+}
+
 
 // Notes to myself:
 // The Stack works with the Last in, First out principle
