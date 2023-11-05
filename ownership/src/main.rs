741a42328377d8871d5d2825f80df67e009e16e9
balljul <juliusball197@gmail.com>
2023-11-05 19:04:44 +0100

    LEARNED about move and clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,10 +2,16 @@ fn main() {
 // Strings:
 
     { // Varablie string_example doesnt exist
-        let mut string_example = String::from("Hello");
-        string_example.push_str(" World");
-        println!("{}", string_example);
+        let mut _string_example = String::from("Hello");
+        _string_example.push_str(" World");
     } // Here the variable goes out of scope and its memory gets automaticly freed
+
+let s1 = String::from("Hello world");
+let s2 = s1;
+let s3 = s2.clone();
+
+println!("s1 is not valid anymore.\ns2: {}\ns3: {}\n", s2, s3);
+
 }
 
 // Notes to myself:
