587ca4832c8b95133879b31d7b5735a540172da6
balljul <juliusball197@gmail.com>
2023-11-05 18:37:20 +0100

    TESTED the String datatype and ADDED some notes

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
 // Strings:
-let string_example = String::from("Hello World");
+let mut string_example = String::from("Hello");
+string_example.push_str(" World");
 
 println!("{}", string_example);
 }
+
+// Notes to myself:
+// The Stack works with the Last in, First out principle
+// The Heap is slower but you get a pointer and you can access specific data
+// literal string are not mutable 
+// The String type is mutable
