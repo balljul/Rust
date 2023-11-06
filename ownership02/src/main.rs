01e711b834f1c59b9daf5caf29915b5bdc6499fc
balljul <juliusball197@gmail.com>
2023-11-06 08:56:27 +0100

    TESTED tuples as return values

--- ownership02/src/main.rs
+++ ownership02/src/main.rs
@@ -1,17 +1,23 @@
 fn main() {
-    let s1 = take_ownership();
+    let s1 = String::from("Hello");
 
-    let s2 = String::from("S2 String");
-    let s3 = give_and_take_ownership(s2);
+    let (s2, len) = calculate_length(s1);
+    
+    println!("The length of {} is: {}", s2, len);
 
-    println!("S1: {}\nS3: {}\n", s1, s3);
 }
 
-fn take_ownership() -> String {
+fn _take_ownership() -> String {
     let string = String::from("S1 String");
     string
 }
 
-fn give_and_take_ownership(string: String) -> String {
+fn _give_and_take_ownership(string: String) -> String {
     string
+}
+
+fn calculate_length(string: String) -> (String, usize) {
+    let length = string.len();
+
+    (string, length)
 }
