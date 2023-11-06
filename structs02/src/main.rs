1acc2b5ab86214946b07e233cfb79b9bb9f97619
balljul <juliusball197@gmail.com>
2023-11-06 18:51:45 +0100

    LEARNED about derrived traits, the debug trait, and nicely structured and sensible code

---
+++ structs02/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect01 = Rectangle {
+        width: 30,
+        height: 40,
+    };
+
+    println!(
+        "The are of the width is: {}",
+        area(&rect01)
+        );
+
+    println!(
+        "The instance looks like this: \n{:#?}",
+        rect01
+        );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+   rectangle.width * rectangle.height 
+}
