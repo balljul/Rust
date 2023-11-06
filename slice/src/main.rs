036fa758aa470c250fcef029c3f00d63353f5b7c
balljul <juliusball197@gmail.com>
2023-11-06 14:02:18 +0100

    ADDED notes about slicing and the range syntax

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let _first_word_end = first_word(&s);
 
     let hello = &s[0..5];
+    let _hello02 = &s[..5];
     let world = &s[6..11];
 
     // println!("The first word in the following Sentence {}\nEnds at position {}", s, first_word_end);
@@ -24,4 +25,6 @@ fn first_word(string: &String) -> usize {
 // When I use the first_word method the value isn´t bound to variable.
 // If i get back the value 5 and change the string it will introduce a wrong result
 // Slices refernce a specific part of a string
-// 
+// The range sytanx allow you to write either &s[x..y]; or if you want to start at index 0 you can just
+// write &s[..y]; You can also ignore the ending_index if you want the reference to go to the last
+// index so for example &s[5..len] is the same as &s[5..];
