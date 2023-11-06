00d2359ec9937cce6988fac7707b81df05cb03a3
balljul <juliusball197@gmail.com>
2023-11-06 20:31:42 +0100

    LEARNED about mehtods and method syntax

--- structs02/src/main.rs
+++ structs02/src/main.rs
@@ -4,25 +4,43 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+       self.width * self.height 
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn height(&self) -> bool {
+        self.height > 0
+    }
+}
+
 fn main() {
-    let scale: u32 = 2;
+    // let scale: u32 = 2;
     let rect01 = Rectangle {
-        width: dbg!(30 * scale),
+        //width: dbg!(30 * scale),
+        width: 30,
         height: 40,
     };
-    dbg!(rect01);
 
-    println!(
-        "The are of the width is: {}",
-        area(&rect01)
-        );
+    if rect01.width() && rect01.height() {
+        println!(
+            "The area of the Rectangle is: {}",
+            rect01.area()
+            );
+    }
 
-    println!(
-        "The instance looks like this: \n{:#?}",
-        rect01
-        );
+    //println!(
+    //    "The instance looks like this: \n{:#?}",
+    //    rect01
+    //    );
+
+    // dbg!(rect01);
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-   rectangle.width * rectangle.height 
-}
+
+// Notes to myself:
+//
