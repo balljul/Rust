935994a6a0feeb09e0655a836265041fae0211dd
balljul <juliusball197@gmail.com>
2023-11-06 09:35:08 +0100

    LEARNED about refernces and how they work

--- ownership02/src/main.rs
+++ ownership02/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let s1 = String::from("Hello");
+    let s1 = String::from("Hello World");
+    let len = calculate_length(&s1);
 
-    let (s2, len) = calculate_length(s1);
+    println!("The length of {} is {}", s1, len);
+}
+
+fn _old_function() {
+    let _s1 = String::from("Hello");
+//    let (s2, len) = calculate_length(s1);
     
-    println!("The length of {} is: {}", s2, len);
+  //  println!("The length of {} is: {}", s2, len);
 
 }
 
@@ -16,8 +22,18 @@ fn _give_and_take_ownership(string: String) -> String {
     string
 }
 
-fn calculate_length(string: String) -> (String, usize) {
+fn _old_calculate_length(string: String) -> (String, usize) {
     let length = string.len();
 
     (string, length)
 }
+
+
+fn calculate_length(string: &String) -> usize {
+    string.len()
+}
+
+// Notes to myself:
+// When useing a reference it is garanteed to point at a value with a specific type in the heap
+// Also when using references you dont transfer ownership
+//
