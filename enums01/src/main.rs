db970c49061cf3dc09f7c4e22822fd0bfb77173e
balljul <juliusball197@gmail.com>
2023-11-08 08:18:00 +0100

    LEARNED about Option<T> and enums

--- enums01/src/main.rs
+++ enums01/src/main.rs
@@ -4,12 +4,17 @@ enum IpAddressKind {
     V6(String),
 }
 
+impl IpAddressKind {
+    fn print(&self) {
+        println!("The Ip-Address is: {:?}", self)
+    }
+}
+
 fn main() {
     let home = IpAddressKind::V4(128, 25, 28, 3);
     let loopback = IpAddressKind::V6(String::from("::1"));
-
-    dbg!(home);
-    dbg!(loopback);
+    loopback.print();
+    home.print();
 }
 
 
