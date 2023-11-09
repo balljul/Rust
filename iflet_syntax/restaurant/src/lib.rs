622993fbbf5f6cc53b76f22614965cc530b54fd0
balljul <juliusball197@gmail.com>
2023-11-09 23:00:37 +0100

    LEARNED about modules and public/private

--- iflet_syntax/restaurant/src/lib.rs
+++ iflet_syntax/restaurant/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+    #[allow(unused)]
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_tabl() {}
+    }
+
+    #[allow(unused)]
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn eat_at_restaurant() {
+    front_of_house::hosting::add_to_waitlist();
 }
