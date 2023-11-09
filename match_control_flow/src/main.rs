94f2813cabd9564be56472767259c144fc7ab90c
balljul <juliusball197@gmail.com>
2023-11-09 13:59:48 +0100

    TESTED enum references in enums and LEARNED about the allow unused option

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -1,25 +1,36 @@
 #[derive(Debug)]
+#[allow(unused)]
+enum UsState {
+    Alabama,
+    Washington,
+    Texas,
+}
+
+#[derive(Debug)]
+#[allow(unused)]
 enum Coin {
-    OCENT,
-    TCENT,
-    FCENT,
-    EURO,
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: &Coin) -> u8 {
     match coin {
-        Coin::OCENT => {
-            println!("You paid a singular cent");
+        Coin::Penny => {
             1
         },
-        Coin::TCENT => 2,
-        Coin::FCENT => 5,
-        Coin::EURO => 100,
+        Coin::Nickel => 2,
+        Coin::Dime => 5,
+        Coin::Quarter(state) => {
+            println!("You got Quarter from {:#?}", state);
+            100
+        },
     }
 }
 
 fn main() {
-    let payment = Coin::OCENT;
+    let payment = Coin::Quarter(UsState::Alabama);
     println!("You payd {} cents", value_in_cents(&payment));
     dbg!(payment);
 }
