97ae8f45df135a145ea32fcf16a66271b25493d1
balljul <juliusball197@gmail.com>
2023-11-09 14:47:46 +0100

    CREATED a new project and TESTED other option

---
+++ match02/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let diceroll = 1;
+    match diceroll {
+        7 => println!("Got a seven"),
+        2 => println!("Got a two"),
+        other => reroll(other),
+    }
+
+    fn reroll(last_number: u8) {
+        println!("Rolled the number {}", last_number)
+    }
+}
