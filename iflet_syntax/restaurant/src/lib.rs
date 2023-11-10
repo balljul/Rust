cbcf250ea1e07a85c97697ed352808f9fcbdc9dc
balljul <juliusball197@gmail.com>
2023-11-10 11:42:04 +0100

    LEARNED about using modules and how to use enums, structures and functions

--- iflet_syntax/restaurant/src/lib.rs
+++ iflet_syntax/restaurant/src/lib.rs
@@ -16,6 +16,37 @@ mod front_of_house {
     }
 }
 
+#[allow(unused)]
+mod back_of_house {
+    pub enum Appetizer {
+        Option01,
+        Option02,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_food: String,
+    }   
+    
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_food: String::from("Oregano"),
+            }
+        }
+    }
+}
+
+#[allow(unused)]
 pub fn eat_at_restaurant() {
     front_of_house::hosting::add_to_waitlist();
+
+    let toast = String::from("tast");
+
+    let mut my_toast = back_of_house::Breakfast::summer(&toast);
+    let mut enum_test = back_of_house::Appetizer::Option01;
+    my_toast.toast = String::from("asdf");
+    // my_toast.seasonal_food = String::from("invalid");
+
 }
