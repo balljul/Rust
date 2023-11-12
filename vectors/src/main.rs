2e0194af26d5f2e26778ddba331ae5fca3ef9572
balljul <juliusball197@gmail.com>
2023-11-12 22:32:08 +0100

    Reading vecs and using it with  Option<T>

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -2,11 +2,23 @@
 fn main() {
     let mut v = vec![];
 
-    v.push(3);
-    v.push(124);
-    v.push(2345);
     v.push(1);
+    v.push(2);
+    v.push(3);
+    v.push(4);
+    v.push(5);
+    v.push(6);
+    v.push(7);
 
-    dbg!(v);
+    dbg!(&v);
+    
+    let third: &i32 = &v[2];
+    println!("Third item is: {}", third);
+
+    let third: Option<&i32> = v.get(10);
+    match third {
+        Some(value) => println!("The 10th value is {value}"),
+        None => println!("No 10th value"),
+    }
+
 }
-
