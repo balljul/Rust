1f6134ccb67f5abc183c9c952a16bdbe28d0e790
balljul <juliusball197@gmail.com>
2023-11-12 22:24:43 +0100

    Vec.push

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,7 +1,12 @@
 #[allow(unused)]
 fn main() {
-    let mut vec01: Vec<i32> = Vec::new();
-    vec01 = vec![0, 123, 45, 1234, 12, 44, 23];
-    dbg!(vec01);
+    let mut v: Vec<i32> = Vec::new();
+
+    v.push(3);
+    v.push(124);
+    v.push(2345);
+    v.push(1);
+
+    dbg!(v);
 }
 
