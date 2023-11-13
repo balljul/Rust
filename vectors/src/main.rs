4078d6332821140277283d6a0e4aedb00db8e3f9
balljul <juliusball197@gmail.com>
2023-11-13 10:29:38 +0100

    TESTED v push in a for in loop with a ranges

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -2,14 +2,9 @@
 fn main() {
     let mut v = vec![];
 
-    v.push(1);
-    v.push(2);
-    v.push(3);
-    v.push(4);
-    v.push(5);
-    v.push(6);
-    v.push(7);
-
+    for num in (1..=20) {
+        v.push(num);
+    }
     dbg!(&v);
     
     let third: &i32 = &v[2];
