197e5569646b196127817e120bf309810c93e129
balljul <juliusball197@gmail.com>
2023-11-13 14:06:14 +0100

    TESTED iteration over a vector

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -2,10 +2,13 @@
 fn main() {
     let mut v = vec![];
 
-    for num in (1..=5) {
+    for num in (1..=12) {
         v.push(num);
     }
-    dbg!(&v);
+
+    for item in &v {
+        println!("{item}");
+    }
     
     let third: &i32 = &v[2];
     println!("Third item is: {}", third);
@@ -14,6 +17,12 @@ fn main() {
     match third {
         Some(value) => println!("The 10th value is {value}"),
         None => println!("No 10th value"),
+    }
+
+    let value_hundred: Option<&i32> = v.get(100);
+    match value_hundred {
+        Some(value) => println!("The 100th value is: {}", value),
+        None => println!("There is no 100th value"),
     }
 
 }
