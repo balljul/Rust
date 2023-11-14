ec508a8dc625212fd539447e8b43d650307c9878
balljul <juliusball197@gmail.com>
2023-11-14 21:19:00 +0100

    TESTED a bit

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,24 @@
+#[allow(unused)]
 fn main() {
-    println!("Hello, world!");
+}
+
+#[allow(unused)]
+fn string_basics() {
+    let data = "Yay";
+    let s = data.to_string();
+
+    {
+        let s = "Initial Commit".to_string(); 
+        let s = String::from("Inital Commit with String::from"); // Same as above
+        dbg!(&s);
+    }
+
+    {
+        // Strings are UTF8 encoded so I can store every properly encoded value in it
+        let s = String::from("Здравствуйте"); 
+        dbg!(&s);
+    }
+
+
+    dbg!(&s);
 }
