39f5a061951c0f34e467fd139e6e726fb8ee97de
balljul <juliusball197@gmail.com>
2023-11-14 21:42:21 +0100

    LEARNED about the format macro

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,13 +1,28 @@
 #[allow(unused)]
 fn main() {
-    let mut s = String::from("Initial ");
-    s.push_str("Commit");
-    s.push(' ');
-    s.push('J');
-    s.push('B');
+    {
+        let mut s = String::from("Initial ");
+        s.push_str("Commit");
+        s.push(' ');
+        s.push('J');
+        s.push('B');
+        // assert_eq!(s, "Initial Commit", "Message One does not equal Message two!");
+    }
 
-    // assert_eq!(s, "Initial Commit", "Message One does not equal Message two!");
+    {
+        let s2 = String::from("ADDED ");
+        let s3 = String::from("Features");
+        let s4 = s2 + &s3; // s2 gets moved so it wont be able to be used again
+    }
+    
+    let s1 = String::from("REWROTE");
+    let s2 = String::from("OLD");
+    let s3 = String::from("BACKEND!!!");
+
+    let s = format!("{} {} {}", s1, s2, s3);
+
     dbg!(&s);
+
 }
 
 #[allow(unused)]
