fc6bc75c92e20ac0efa1457e96e71b5f93b3105a
balljul <juliusball197@gmail.com>
2023-11-16 10:22:26 +0100

    TESTED the Result enum

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,9 +1,18 @@
 use std::fs::File;
-use std::fs::read_to_string;
 
 #[allow(unused)]
 fn main() {
-    let file_result = File::open("text.txt");
-    let file_content = read_to_string("text.txt");
-    dbg!(file_content);
+    result_test();
+}
+
+#[allow(unused)]
+fn result_test() {
+    let file_result = File::open("text.tst");
+
+    let file = match file_result {
+        Ok(file) => file,
+        Err(error) => panic!("\nFollowing error when trying to open the file: \n{}\n", error),
+    };
+    
+
 }
