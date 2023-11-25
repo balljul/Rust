dff04d5c43841eddd1c30761f69329c738b198a0
balljul <juliusball197@gmail.com>
2023-11-25 20:53:36 +0100

    TESTED a bit

--- error02/src/main.rs
+++ error02/src/main.rs
@@ -1,11 +1,9 @@
 use std::net::IpAddr;
 
 fn main() {
-
     let localhost: IpAddr = "127.0.0.1"
         .parse()
         .expect("Hardcoded ip address should be valid");
 
     dbg!(&localhost);
-    println!("Yay");
 }
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,10 +2,26 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Number must between 1 and 100");
+        }
+        Guess {value}
+    }
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess a number!");
     
-    let number_to_guess = rand::thread_rng().gen_range(1..=5); 
+    let number_to_guess = rand::thread_rng().gen_range(1..=100); 
 
     loop {
         let mut guess = String::new();
@@ -17,9 +33,10 @@ fn main() {
             .expect("Failed to read your input");
 
         let guess: u32 = match guess.trim().parse() {
-                Ok(num) => num,
-                Err(_) => continue,
-            };
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
 
         match guess.cmp(&number_to_guess){
             Ordering::Less => println!("Guess Higher"),
@@ -30,4 +47,5 @@ fn main() {
             }
         } 
     }
+
 }
