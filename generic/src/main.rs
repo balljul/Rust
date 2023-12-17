54a5721bbf48657e2b44e808ad5937d379905fe0
balljul <juliusball197@gmail.com>
2023-12-17 21:30:20 +0100

    LEARNED about generics in structs

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -6,18 +6,29 @@ fn largest(list: &[i32]) -> &i32 {
             largest = num;
         }
     }
-
     &largest
+}
+
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
 
     let my_list = [23, 3732, 34, 532, 99, 999, 9999, 10450];
-    
     println!("{}", largest(&my_list));
 
     let my_list = vec![2,3,4,5,6,70,8];
-
     println!("{}", largest(&my_list));
     
+    let test = Point {x:5, y:8};
+    dbg!(test);
+    
+    let test = Point {x:5.0, y:8.000};
+    dbg!(test);
+    
+    let test = Point {x:5, y:8.000};
+    dbg!(test);
 }
