9646f5c911cf704250b0cefc6657db705cfcd0a9
balljul <juliusball197@gmail.com>
2024-02-11 18:18:17 +0100

    RELEARNING rust

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+struct Color(i32,i32,i32);
+
+#[derive(Debug)]
+struct Point(u64,u64,u64);
+
+#[derive(Debug)]
+struct AlwaysEqual;
+
 fn main() {
-    println!("Hello, world!");
+    let black = Color(255,255,255);
+    let origin = Point(0,0,0);
+    let unit = AlwaysEqual;
+
+    dbg!(black, origin, unit);
 }
