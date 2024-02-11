934de0c70e61b2662d5adf8aee9a60c89041763d
balljul <juliusball197@gmail.com>
2024-02-11 22:02:18 +0100

    Structs

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,16 +1,63 @@
 #[derive(Debug)]
+#[allow(unused)]
+// tuple structs
 struct Color(i32,i32,i32);
 
 #[derive(Debug)]
-struct Point(u64,u64,u64);
+#[allow(unused)]
+// field structs
+struct User {
+    field1: bool,
+    field2: u128,
+}
 
 #[derive(Debug)]
+#[allow(unused)]
+// unit-like structs
 struct AlwaysEqual;
 
-fn main() {
-    let black = Color(255,255,255);
-    let origin = Point(0,0,0);
-    let unit = AlwaysEqual;
+// Notes end here
 
-    dbg!(black, origin, unit);
+#[derive(Debug)]
+#[allow(unused)]
+struct Rectangle {
+    width: u32,
+    heigth: u32
 }
+
+#[allow(unused)]
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn area(&self) -> u32 {
+        self.width * self.heigth
+    }
+    fn compare(&self, other: Rectangle) -> bool {
+        self.area() > other.area()
+    }
+    fn sqaure(size: u32) -> Self {
+        Self {
+            width: size,
+            heigth: size,
+        }
+    }
+}
+
+#[allow(unused)]
+fn main() {
+    const SCALE: u32 = 2;
+    let rect1 = Rectangle {
+        width: 30 * SCALE,
+        heigth: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 18 * SCALE,
+        heigth: 50,
+    };
+
+    let rect3 = Rectangle::sqaure(115);
+    dbg!(&rect3);
+}
+
